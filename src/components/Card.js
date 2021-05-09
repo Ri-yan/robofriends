6ab6f812883f0e380f402cab063fcdec2cf26cbc
Ri-yan <74947287+Ri-yan@users.noreply.github.com>
2021-05-09 19:33:18 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
  
  const Card = (props) => {
  	return (
- 		<div className='bg-light-green dib br5 pa2 na1 grow bw2 shadow-6 '>
+ 		<div className='bg-light-green dib br3 pa4 ma2 grow bw3 shadow-6'>
  		<img  alt='robots' src={`https://robohash.org/${id}`} >
 //'https://robohash.org/test?200x200'/
  			<div > 
